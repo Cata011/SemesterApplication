414fa5d9fe183859509fad828119a7eaa5889972
Cata011 <alex_catalin1700@yahoo.com>
2021-04-07 13:05:30 +0200

    zz1

--- app/src/main/java/com/example/semesterapplication/ListActivity.java
+++ app/src/main/java/com/example/semesterapplication/ListActivity.java
@@ -1,14 +1,18 @@
 package com.example.semesterapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class ListActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_list);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
     }
 }
